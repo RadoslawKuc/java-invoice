74a246569d59a90cc2a4f3ce6d94d692d3cdc2f8
RadekKuc <radoslaw.kuc@op.pl>
2023-02-11 12:12:38 +0100

    Początek invoice

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,44 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    //private Collection<Product> products = new ArrayList<>();
+    private Map<Product, Integer> products = new HashMap<>();
 
     public void addProduct(Product product) {
         // TODO: implement
+        this.products.add(product);
     }
 
     public void addProduct(Product product, Integer quantity) {
         // TODO: implement
+        for (int i=0; i<quantity; i++){
+            this.products.add(product);
+        }
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+
+        if (products == null)
+            return BigDecimal.ZERO;
+        return this.products.stream()
+                .map(product -> product.getPrice())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     public BigDecimal getTax() {
-        return null;
+
+        return BigDecimal.ZERO;
     }
 
     public BigDecimal getTotal() {
-        return null;
+
+        return BigDecimal.ZERO;
     }
 }
